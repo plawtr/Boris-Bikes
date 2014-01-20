43c24bfd869d974ba7f0da2a261eabd2f8355ee9
Andrey Krahmal <coolrighthere@yahoo.com>
2014-01-20 19:59:48 +0000

    Add bike container functionality

--- lib/bike.rb
+++ lib/bike.rb
@@ -3,7 +3,6 @@ class Bike
 	def initialize 
 		fix
 	end
-
 	
 	def broken?
 		@broken
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,5 +1,5 @@
-require_relative "../lib/bike.rb"
-require_relative "../lib/docking_station.rb"
+require_relative "../lib/bike"
+
 
 describe Bike do 
 
@@ -21,45 +21,6 @@ describe Bike do
     end
 end
 
-describe DockingStation do
 
-	let(:bike) {Bike.new}
-	let(:station) {DockingStation.new(:capacity => 20)}
 
-	def fill_station(station)
-		20.times { station.dock(Bike.new)}
-	end
-
-	it "should accept a bike" do
-		expect(station.bike_count).to eq(0)
-		station.dock(bike) 
-		expect(station.bike_count).to eq(1)
-	end
-
-	it "should release a bike" do
-		station.dock(bike)
-		station.release(bike)
-		expect(station.bike_count).to eq(0)
-	end
-
-	it "should know when it is full" do
-		expect(station).not_to be_full
-		fill_station station
-		expect(station).to be_full
-	end
-
-	it "should not accept a bike if it is full" do
-		fill_station station
-		expect(lambda {station.dock(bike)}).to raise_error(RuntimeError)
-	end
-
-	it "should provide the list of available bikes" do
-		working_bike, broken_bike = Bike.new, Bike.new
-		broken_bike.break
-		station.dock(working_bike)
-		station.dock(broken_bike)
-		expect(station.available_bikes).to eq([working_bike])
-	end
-	
-end
 
